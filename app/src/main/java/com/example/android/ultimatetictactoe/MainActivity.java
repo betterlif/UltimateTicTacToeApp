59ab0d09bc63037636ced88de16c470942a2b975
Saishrey <86342945+Saishrey@users.noreply.github.com>
2022-06-23 21:50:39 +0530

    Tic-Tac-Toe App

--- app/src/main/java/com/example/android/ultimatetictactoe/MainActivity.java
+++ app/src/main/java/com/example/android/ultimatetictactoe/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -41,27 +42,40 @@ public class MainActivity extends AppCompatActivity {
     private volatile int playerOneScore = 0, playerTwoScore = 0;
 
     private boolean isAIselected;
+
+    private String difficultyLevel;
+
     private Vibrator myVib;
 
     private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         Bundle extras = getIntent().getExtras();
         isAIselected = extras.getBoolean("AI_SELECTED");
+
         if(isAIselected) {
-            setTitle("Play against AI");
             player1 = "AI";
             player2 = "You";
+            difficultyLevel = extras.getString("DIFFICULTY_LEVEL");
+            if(difficultyLevel.equals("EASY")) {
+                setTitle("Play against AI - Easy");
+                setTheme(R.style.AIEasyAppTheme);
+            }
+            else {
+                setTitle("Play against AI - Hard");
+                setTheme(R.style.AIHardAppTheme);
+            }
         }
         else {
             setTitle("Multiplayer");
+            setTheme(R.style.MPAppTheme);
             player1 = "Player 1";
             player2 = "Player 2";
         }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         playerOneName = findViewById(R.id.player_one_name);
         playerTwoName = findViewById(R.id.player_two_name);
@@ -257,7 +271,7 @@ public class MainActivity extends AppCompatActivity {
         if(checkPlayerWin()) {
             playerTwoScore++;
             setScore();
-            WinDialog winDialog = new WinDialog(MainActivity.this, player2 + " win.", MainActivity.this);
+            WinDialog winDialog = new WinDialog(MainActivity.this, "X wins.", MainActivity.this);
             winDialog.setCancelable(false);
             winDialog.show();
         }
@@ -269,11 +283,18 @@ public class MainActivity extends AppCompatActivity {
         else {
             changePlayerTurn(1);
             totalSelectedBoxes++;
-            mHandler.postDelayed(computersTurn, 1000);
+
+            if(difficultyLevel.equals("EASY")) {
+                mHandler.postDelayed(computersTurnEasy, 500);
+            }
+            else {
+                mHandler.postDelayed(computersTurnHard, 500);
+            }
+
         }
     }
 
-    private Runnable computersTurn = new Runnable() {
+    private Runnable computersTurnEasy = new Runnable() {
         @Override
         public void run() {
             List<Integer> moves = getAvailableMoves();
@@ -314,7 +335,7 @@ public class MainActivity extends AppCompatActivity {
             if(checkPlayerWin()) {
                 playerOneScore++;
                 setScore();
-                WinDialog winDialog = new WinDialog(MainActivity.this, player1 + " wins.", MainActivity.this);
+                WinDialog winDialog = new WinDialog(MainActivity.this, "X wins.", MainActivity.this);
                 winDialog.setCancelable(false);
                 winDialog.show();
             }
@@ -330,6 +351,209 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private Runnable computersTurnHard = new Runnable() {
+
+        private int player = 1, opponent = 2;
+
+        @Override
+        public void run() {
+            int bestMove;
+            if(totalSelectedBoxes == 1) {
+                Random rand = new Random();
+                List<Integer> moves = getAvailableMoves();
+                bestMove = moves.get(rand.nextInt(moves.size()));
+            }
+            else {
+                bestMove = findBestMove(boxPositions);
+            }
+
+            boxPositions[bestMove] = playerTurn;
+
+            switch(bestMove) {
+                case 0:
+                    image1.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 1:
+                    image2.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 2:
+                    image3.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 3:
+                    image4.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 4:
+                    image5.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 5:
+                    image6.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 6:
+                    image7.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 7:
+                    image8.setImageResource(R.drawable.cross_icon);
+                    break;
+                case 8:
+                    image9.setImageResource(R.drawable.cross_icon);
+            }
+
+            if(checkPlayerWin()) {
+                playerOneScore++;
+                setScore();
+                WinDialog winDialog = new WinDialog(MainActivity.this, "X wins.", MainActivity.this);
+                winDialog.setCancelable(false);
+                winDialog.show();
+            }
+            else if(totalSelectedBoxes == 9) {
+                WinDialog winDialog = new WinDialog(MainActivity.this, "Match Draw.", MainActivity.this);
+                winDialog.setCancelable(false);
+                winDialog.show();
+            }
+            else {
+                changePlayerTurn(2);
+                totalSelectedBoxes++;
+            }
+
+        }
+
+        private Boolean isMovesLeft(int board[]) {
+            for(int box: board) {
+                if(box == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // This is the evaluation function as discussed
+        // in the previous article ( http://goo.gl/sJgv68 )
+        int evaluate(int board[]) {
+            for(int i = 0; i < combinationsList.size(); i++) {
+                final int[] combination = combinationsList.get(i);
+
+                if(board[combination[0]] == board[combination[1]] &&  board[combination[1]] == board[combination[2]]) {
+                    if(board[combination[0]] == player) {
+                        return +10;
+                    }
+                    else {
+                        return -10;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        // This is the minimax function. It considers all
+        // the possible ways the game can go and returns
+        // the value of the board
+        int minimax(int board[], int depth, Boolean isMax) {
+            int score = evaluate(board);
+
+            // If Maximizer has won the game
+            // return his/her evaluated score
+            if (score == 10)
+                return score;
+
+            // If Minimizer has won the game
+            // return his/her evaluated score
+            if (score == -10)
+                return score;
+
+            // If there are no more moves and
+            // no winner then it is a tie
+            if (!isMovesLeft(board))
+                return 0;
+
+            // If this maximizer's move
+            if(isMax) {
+                int best = -1000;
+
+                // Traverse all cells
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3 && (i * 3 + j) < board.length; j++) {
+                        // Check if cell is empty
+                        if (board[i * 3 + j] == 0) {
+                            // Make the move
+                            board[i * 3 + j] = player;
+
+                            // Call minimax recursively and choose
+                            // the maximum value
+                            best = Math.max(best, minimax(board, depth + 1, false));
+
+                            // Undo the move
+                            board[i * 3 + j] = 0;
+                        }
+                    }
+                }
+                return best;
+            }
+
+            // If this minimizer's move
+            else
+            {
+                int best = 1000;
+
+                // Traverse all cells
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3 && (i * 3 + j) < board.length; j++)
+                    {
+                        // Check if cell is empty
+                        if (board[i * 3 + j] == 0)
+                        {
+                            // Make the move
+                            board[i * 3 + j] = opponent;
+
+                            // Call minimax recursively and choose
+                            // the minimum value
+                            best = Math.min(best, minimax(board, depth + 1, true));
+
+                            // Undo the move
+                            board[i * 3 + j] = 0;
+                        }
+                    }
+                }
+                return best;
+            }
+        }
+
+        // This will return the best possible
+        // move for the player
+        int findBestMove(int board[]) {
+            int bestVal = -1000;
+            int bestMove = -1;
+
+            // Traverse all cells, evaluate minimax function
+            // for all empty cells. And return the cell
+            // with optimal value.
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3 && (i * 3 + j) < board.length; j++) {
+                    // Check if cell is empty
+                    if (board[i * 3 + j] == 0) {
+                        // Make the move
+                        board[i * 3 + j] = player;
+
+                        // compute evaluation function for this
+                        // move.
+                        int moveVal = minimax(board, 0, false);
+
+                        // Undo the move
+                        board[i * 3 + j] = 0;
+
+                        // If the value of the current move is
+                        // more than the best value, then update
+                        // best/
+                        if (moveVal > bestVal) {
+                            bestMove = i * 3 + j;
+                            bestVal = moveVal;
+                        }
+                    }
+                }
+            }
+            return bestMove;
+        }
+    };
 
     private void performAction(ImageView imageView, int selectBoxPosition) {
         boxPositions[selectBoxPosition] = playerTurn;
@@ -339,7 +563,7 @@ public class MainActivity extends AppCompatActivity {
             if(checkPlayerWin()) {
                 playerOneScore++;
                 setScore();
-                WinDialog winDialog = new WinDialog(MainActivity.this, player1 + " wins.", MainActivity.this);
+                WinDialog winDialog = new WinDialog(MainActivity.this, "X wins.", MainActivity.this);
                 winDialog.setCancelable(false);
                 winDialog.show();
             }
@@ -360,7 +584,7 @@ public class MainActivity extends AppCompatActivity {
             if(checkPlayerWin()) {
                 playerTwoScore++;
                 setScore();
-                WinDialog winDialog = new WinDialog(MainActivity.this, player2 + " wins.", MainActivity.this);
+                WinDialog winDialog = new WinDialog(MainActivity.this, "O wins.", MainActivity.this);
                 winDialog.setCancelable(false);
                 winDialog.show();
             }
@@ -371,7 +595,6 @@ public class MainActivity extends AppCompatActivity {
             }
             else {
                 changePlayerTurn(1);
-
                 totalSelectedBoxes++;
             }
         }
@@ -427,13 +650,20 @@ public class MainActivity extends AppCompatActivity {
         playerTwoLayout.setBackgroundResource(R.drawable.round_back_dark_blue);
 
         if(isAIselected) {
-            mHandler.postDelayed(computersTurn, 400);
+            if(difficultyLevel.equals("EASY")) {
+                mHandler.postDelayed(computersTurnEasy, 500);
+            }
+            else {
+                mHandler.postDelayed(computersTurnHard, 500);
+            }
         }
     }
 
     private void setScore() {
         scoreCard.setText(playerOneScore + " - " + playerTwoScore);
     }
+
+
 
     public void goToMenu() {
         Intent intent = new Intent(MainActivity.this, MainMenu.class);
--- app/src/main/java/com/example/android/ultimatetictactoe/MainMenu.java
+++ app/src/main/java/com/example/android/ultimatetictactoe/MainMenu.java
@@ -4,26 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        final Button aiButton = findViewById(R.id.play_against_ai);
+        final Button aiButtonEasy = findViewById(R.id.play_against_ai_easy);
+        final Button aiButtonHard = findViewById(R.id.play_against_ai_hard);
         final Button multiplayerButton = findViewById(R.id.multiplayer_button);
 
-        aiButton.setOnClickListener(new View.OnClickListener() {
+        aiButtonEasy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainMenu.this, MainActivity.class);
                 intent.putExtra("AI_SELECTED", true);
+                intent.putExtra("DIFFICULTY_LEVEL", "EASY");
+                startActivity(intent);
+            }
+        });
+
+        aiButtonHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenu.this, MainActivity.class);
+                intent.putExtra("AI_SELECTED", true);
+                intent.putExtra("DIFFICULTY_LEVEL", "HARD");
                 startActivity(intent);
             }
         });
@@ -31,11 +44,11 @@ public class MainMenu extends AppCompatActivity {
         multiplayerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(MainMenu.this, MainActivity.class);
                 intent.putExtra("AI_SELECTED", false);
                 startActivity(intent);
             }
         });
-
     }
 }
